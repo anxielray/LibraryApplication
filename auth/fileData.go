2f3fecac989f39c81ff1ff353f1665376734f262
anxielray <anxielworld@gmil.com>
2024-12-23 20:25:31 +0300

    [General]: Add file obtaining the file content from the authentication data files

---
+++ auth/fileData.go
@@ -0,0 +1,24 @@
+package auth
+
+import (
+	"fmt"
+	"log"
+	"os"
+)
+
+func FileData() (*os.File, *os.File) {
+
+	//open the files in the directory data
+	fmt.Println("Could not open data/users.txt")
+	Userfile, err := os.Open("auth/data/users.txt")
+	if err != nil {
+		log.Fatalf("failed to open file: %s", err)
+	}
+
+	Membersfile, err := os.OpenFile("auth/data/members.txt", os.O_APPEND|os.O_WRONLY, 0644)
+	if err != nil {
+		fmt.Println("Error opening file:", err)
+		os.Exit(1)
+	}
+	return Userfile, Membersfile
+}
