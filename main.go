a803ef73e32ff89bcda99ea1146c2b1679fe35f2
anxielray <anxielworld@gmail.com>
2024-12-24 05:22:01 +0300

    [Modify]: Modify the file with the main source flie containing the handler registrations and some handlers as well

--- main.go
+++ main.go
@@ -1,26 +1,28 @@
 package main
 
 import (
-	auth "Anxiel-Archives/auth"
 	"fmt"
 	"log"
 	"net/http"
 	"text/template"
+
+	auth "Anxiel-Archives/auth"
+	lib "Anxiel-Archives/library"
+	prf "Anxiel-Archives/profile"
 )
 
 func main() {
-
 	// Serve the login form
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		http.ServeFile(w, r, "templates/login.html")
 	})
 	http.HandleFunc("/login", auth.HandleLogin)
 
-	//serve the registration form
+	// serve the registration form
 	http.HandleFunc("/register", func(w http.ResponseWriter, r *http.Request) {
 		http.ServeFile(w, r, "templates/registration.html")
 	})
-	http.HandleFunc("/register/", auth.HandleRegistration)
+	http.HandleFunc("/registration", auth.HandleRegistration)
 
 	// Handle the dashboard route
 	http.HandleFunc("/dashboard", func(w http.ResponseWriter, r *http.Request) {
@@ -36,12 +38,18 @@ func main() {
 		}
 	})
 
+	// serve the functionality page of the program
+	http.HandleFunc("/library", lib.HandleLibrary)
+
+	// serve the profile page
+	http.HandleFunc("/profile", prf.HandleProfile)
+
 	fmt.Println("Server is running on localhost:9999")
 	if err := http.ListenAndServe(":9999", nil); err != nil {
 		log.Fatal(err)
 	}
 
-	//defer close the open files
+	// defer close the open files
 	U, M := auth.FileData()
 	defer U.Close()
 	defer M.Close()
